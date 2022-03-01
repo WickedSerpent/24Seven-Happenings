55e988cad5a43592b1dd691d9b34d6b23fa25fed
Atlefar <46597602+Atlefar@users.noreply.github.com>
2022-03-01 10:21:44 +0100

    moved model.js to js folder

---
+++ js/model.js
@@ -0,0 +1,21 @@
+const model = [
+    page = 'hovedSide', /* 'Arrangementer', 'users', 'unique'*/
+    createPerson = {
+        personId: '',
+        navn: '',
+        lastName: '',
+        addValue: '',
+        poeng: '',
+    },
+    inputs = {
+        addHappening: '',
+        addPerson: '',
+        slider: '',
+        searchPersons
+    },
+    people = [
+        {person: id=0, name: Atle, lastName: Atleson, value: '1', poeng: 10},
+        {person: id=1, name: Kristian, lastName: Kristianson, value: '2', poeng: 10},
+        {person: id=2, name: Mats, lastName: Matsson, value: '3', poeng: 10},
+    ],
+]
