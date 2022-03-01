4a0c70a2ac71e52c21acd8a13c926f7fed3fa7c9
WickedSerpent <74629174+WickedSerpent@users.noreply.github.com>
2022-03-01 13:08:32 +0100

    la updateViewHome func i controller
    ... da den bare kontrollerer hvordan gettall funksjonene (view) fungerer.

--- js/homeView.js
+++ js/homeView.js
@@ -1,17 +1,3 @@
-function updateViewHome() {
-    document.getElementById('app').innerHTML = /*html*/`
-        ${createMenuHtml()} 
-        <h1>Her vises alle arrangementer!</h1>
-        <div class="boxOne">
-        ${getAllHappenings()}
-        </div>
-        <div class="boxTwo">
-        ${getAllUsers()}
-        </div>
-    `;
-
-}
-
 
 function getAllHappenings(){
     let html = '';
