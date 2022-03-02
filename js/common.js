568af0c96c03fb5fd7b96755f5b30d7268351eb2
MatsSiGETacademy <98326259+MatsSiGETacademy@users.noreply.github.com>
2022-03-02 09:47:46 +0100

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -24,41 +24,20 @@ function getMaxHappeningId(){
     return id;
 }
 
-function findHappeningById(id) {
-    for (let happening of model.data.happenings) {
-        if (happening.id === id) return happening;
-    }
-    return null;
-
-}
-
-function findHappeningIndexById(id) {
-    for (let i = 0; i < model.data.happenings.length; i++) {
-        let happening = model.data.happenings[i];
-        if (happening.id === id) return i;
-    }
-    return null;
-
-}
-
 function findUserById(id){
     for(let user of model.data.users){
         if(user.id === id) return user;
     } 
     return null;
- 
  }
 
- function findUserIndexById(id){
-    for(let i = 0; i < model.data.users.length; i++){
-        let user = model.data.users[i];
-        if(user.id === id) return i;
+ function findHappeningById(id){
+    for(let happening of model.data.happenings){
+        if(happening.id === id) return happening;
     } 
     return null;
- 
  }
 
  function getLowestPoints(){
      
-
 }
