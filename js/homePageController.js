bd1e9465541ac6733e9f980f2ede7512743acca9
WickedSerpent <deilrind@live.no>
2022-03-07 15:02:42 +0100

    mandag ettermiddag

--- js/homePageController.js
+++ js/homePageController.js
@@ -92,7 +92,10 @@ function sum(){
     userPoints = model.data.userPoints;
     let sum = 0;
     for(points in userPoints){
-        sum += userPoints[points];
+            
         // return points;
     }
+    const summed = userPoints.reduce((points, userPoints) => points + (userPoints[points], 0));
+    return summed; 
+    console.log(summed);
 }
