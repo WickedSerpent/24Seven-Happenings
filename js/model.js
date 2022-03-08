d7d567bacc4486cc8b22f369c90b055d944500ec
KSvarstad <97854409+KSvarstad@users.noreply.github.com>
2022-03-08 08:41:59 +0100

    Update model.js

--- js/model.js
+++ js/model.js
@@ -50,15 +50,48 @@ const model = {
 
         selectAll: false,
         users: [
-        
+            {id: 1, name:'Geir', isSelected: false, },
+            {id: 2, name:'Eskil', isSelected: false, },
+            {id: 3, name:'Terje', isSelected: false, },
         ],
 
         userPoints: [
-            
+            {userId: 1, happeningId: 1, points: 2},
+            {userId: 2, happeningId: 1, points: 2},
+            {userId: 3, happeningId: 1, points: 2},
+            {userId: 1, happeningId: 2, points: 85},
+            {userId: 2, happeningId: 2, points: 85},
+            {userId: 3, happeningId: 2, points: 85},
+            {userId: 1, happeningId: 3, points: 12},
+            {userId: 2, happeningId: 3, points: 12},
+            {userId: 3, happeningId: 3, points: 12},
+            {userId: 1, happeningId: 4, points: 8},
+            {userId: 2, happeningId: 4, points: 8},
+            {userId: 3, happeningId: 4, points: 8},
         ],
         
+
         happenings: [
-            
+            {
+                id: 1, 
+                name:'Kahoot',
+                isSelected: false,
+            }, 
+            {
+                id: 2, 
+                name:'Rense kaffemaskin',
+                isSelected: false,
+            }, 
+            {
+                id: 3, 
+                name:'Ta ut av oppvaskmaskin',
+                isSelected: false,
+            }, 
+            {
+                id: 4, 
+                name:'Sette inn i oppvaskmaskin',
+                isSelected: false,
+            }, 
         ],
 
         
