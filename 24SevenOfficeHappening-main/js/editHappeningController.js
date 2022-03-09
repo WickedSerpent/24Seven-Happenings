01848b01f1df6297553212839b877e10c2de6b34
MatsSiGETacademy <98326259+MatsSiGETacademy@users.noreply.github.com>
2022-03-09 14:07:51 +0100

    Delete 24SevenOfficeHappening-main directory

--- 24SevenOfficeHappening-main/js/editHappeningController.js
+++
@@ -1,6 +0,0 @@
-function editHappening() {
-    const happening = getHappeningById(model.inputs.editHappening.happeningId);
-    happening.name = model.inputs.editHappening.name;
-    model.app.page = 'home';
-    updateView();
-}
--- 24SevenOfficeHappening-main/js/resetHappeningsController.js
+++
@@ -1,7 +0,0 @@
-function resetHappenings(){
-    if (! model.inputs.resetDoneHappenings.areYouSure) return;
-    model.data.doneHappenings = []
-    model.inputs.resetDoneHappenings.areYouSure = false;
-    model.app.page = 'happening'
-    updateView()
-}
