ed78230c07c3f45c5b3443d05e27ce4a50b58c7a
MatsSiGETacademy <98326259+MatsSiGETacademy@users.noreply.github.com>
2022-03-09 14:08:10 +0100

    test

--- 24SevenOfficeHappening-main/js/editUserController.js
+++
@@ -1,6 +0,0 @@
-function editUser() {
-    const user = getUserById(model.inputs.editUser.userId);
-    user.name = model.inputs.editUser.name;
-    model.app.page = 'home';
-    updateView();
-}
--- 24SevenOfficeHappening-main/resources/updateUserHappeningsAvJoakim.js
+++
@@ -1,8 +0,0 @@
-function updateUserHappenings(){
-    const users = model.data.users;
-    for(let i=0; i < users.length; i++){
-        for(let j = 0; j < model.data.happenings.length; j++){
-        model.data.happenings[j].push(happening)
-        }
-    }
-}   
