c6a3e331d5e47aceec0907a299533ce438e83829
WickedSerpent <deilrind@live.no>
2022-03-15 13:00:51 +0100

    reset til admin

--- js/resetHappeningsController.js
+++ js/resetHappeningsController.js
@@ -3,9 +3,11 @@ function resetHappenings(){
     if (! model.inputs.resetDoneHappenings.areYouSure) return;
     model.data.doneHappenings = []
     model.inputs.resetDoneHappenings.areYouSure = false;
-    model.app.page = 'happening'
-    updateView()
+    model.app.page = 'happeningsAdmin'
+    updateAdminView()
 }
+
+
 /* Jeg vil prøve å targete happeningID via checked checkboxes for å gjøre resetten unik til den happeningen, 
 kan kanskje ha en egen funk til det tenker jeg så vi kan knytte de til separerte knapper*/
 
