64a3c63c894573f1fd425a0efc25eed5026589ab
Atlefar <46597602+Atlefar@users.noreply.github.com>
2022-03-18 12:33:57 +0100

    Gjorde om if setting til ternary

--- js/loginController.js
+++ js/loginController.js
@@ -1,11 +1,6 @@
 function validate(){
+    let password = document.getElementById("password").value;
 
-let password = document.getElementById("password").value;
-
-if (password == "temp123"){
-model.app.page = 'admin'; 
-} else {
-    alert("Feil passord!")
-}
-updateAdminView();
+    password == "temp123" ? model.app.page = 'admin' : alert("Feil passord!");
+    updateAdminView();
 }
