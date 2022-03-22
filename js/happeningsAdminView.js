d59106d79eb1e32f3ca8a7fc1f8466cfedc4658f
KSvarstad <97854409+KSvarstad@users.noreply.github.com>
2022-03-22 10:58:02 +0100

    Update happeningsAdminView.js

--- js/happeningsAdminView.js
+++ js/happeningsAdminView.js
@@ -102,7 +102,7 @@ function adminGetDoneHappening() {
       <h3>Trekning - <span style="color: #FF5733;">${doneHappening.name}</span></h3>
       <h3>Trukket person - <span style="color: #6AB334;">${doneHappening.userDrawn}</span></h3>
       <h3>Kommentar: <span style="font-weight: 500;">${doneHappening.comment}</span></h3>
-      <div id="details--${doneHappening.id}">
+      <div>
       <h3>Trukket fra disse personene med færrest poeng:<br> 
       <span style="color: #0075ff;">${createTextList(doneHappening.participants)}</span></h3>
       <h4>Trukket: ${dayName} ${dateText}</h4>
