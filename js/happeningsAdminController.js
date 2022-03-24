4211545ef4ee8c65e8af0d5793d12b9e86f3d862
KSvarstad <97854409+KSvarstad@users.noreply.github.com>
2022-03-24 23:44:56 +0100

    updates

--- js/happeningsAdminController.js
+++ js/happeningsAdminController.js
@@ -42,11 +42,12 @@ function addCommentAdmin(id){
     }
     else {
         let comment = {}
+        comment.commentTime = getNowForStorage()
         comment.commentId = getMaxCommentIdDoneHappening(id) + 1
         comment.comment = model.inputs.comment
         happening.comments.push(comment)
         model.inputs.comment = ''
-        updateAdminView()
+        updateView()
     }
 }
 
